e100279a35afdc95c81ec3e5292ae6d302cc919a
Gabriel <gpopoff504@gmail.com>
2026-02-27 10:12:07 -0600

    Clase comands

--- src/laboratorio_5_comandos/Gui.java
+++ src/laboratorio_5_comandos/Gui.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.text.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.*; 
 
 
 public class Gui extends JFrame {
