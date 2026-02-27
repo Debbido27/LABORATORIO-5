3448e656ace3e6a6bc52295f035e4410fcb38cd5
Gabriel <gpopoff504@gmail.com>
2026-02-27 08:38:06 -0600

    Update

---
+++ src/laboratorio_5_comandos/comandosp2.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio_5_comandos;
+import java.io.BufferedReader;
+
+/**
+ *
+ * @author gpopo
+ */
+public class comandosp2 {
+    
+}
