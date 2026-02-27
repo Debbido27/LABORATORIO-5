9ec69858064c0788c504bdeb1a18c83b0522333e
Debbido27 <davidmedina272004@gmail.com>
2026-02-27 08:46:02 -0600

    Se declaro el scanner para poder recibir los datos, tambien el file, que se hara en la direccion actual, para que pueda estar en el poryecto, y el metodo iniciar

--- src/laboratorio_5_comandos/CMD1_5.java
+++ src/laboratorio_5_comandos/CMD1_5.java
@@ -1,7 +1,26 @@
 
 package laboratorio_5_comandos;
 
+import java.io.File;
+import java.util.Scanner;
+
 
 public class CMD1_5 {
+    Scanner entrada = new Scanner(System.in);
+    private File dirActual;
+   
+    public  CMD1_5(){
+        this.dirActual=new File(System.getProperty("user.dir"));
+        
+    }
     
+    public void iniciar(){
+        String comando;
+        boolean corriendo = true;
+        
+        System.out.println("CONSOLA CMD");
+        System.out.println("Escribe 'EXIT' para salir");
+        System.out.println();
+        
+    }
 }
